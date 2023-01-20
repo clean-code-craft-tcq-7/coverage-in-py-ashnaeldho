48281fd837b407fb0253461266879a738a922989
ashnaeldho <ashnaeldho12@gmail.com>
2023-01-20 14:43:01 +0530

    Create printer.py

---
+++ printer.py
@@ -0,0 +1,6 @@
+def print_send_controller(header,breachType):
+    print(f'{header}, {breachType}')
+
+def print_send_email(recepient,text):
+    print(f'To: {recepient}')
+    print("Hi, the temperature is " + text)
