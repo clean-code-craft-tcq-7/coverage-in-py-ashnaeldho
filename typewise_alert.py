350f4fa7f6837e0aa8dcf2f1326a15c05c8a7176
ashnaeldho <ashnaeldho12@gmail.com>
2023-01-20 14:45:32 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,25 +1,12 @@
 
-from limits import LIMITS
-from send_alerts import send_to_controller, send_to_email
+from sender import *
+from breach_detecter import *
+from breach_classifier import *
 
-
-def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
-  if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
-
-
-
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  Flg = 0
-  lowerlimit, upperlimit = LIMITS[batteryChar['coolingType']]
+def check_and_alert(alertTarget, batteryChar, temperatureInC,controllerFunctionPointer=send_to_controller,emailFunctionPointer=send_to_email):
   breachType =\
-    infer_breach(temperatureInC, lowerlimit, upperlimit)
+    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
   if alertTarget == 'TO_CONTROLLER':
-    Flg= send_to_controller(breachType)
-    return Flg
+    controllerFunctionPointer(breachType)
   elif alertTarget == 'TO_EMAIL':
-    Flg= send_to_email(breachType)
-    return Flg
+    emailFunctionPointer(breachType)
